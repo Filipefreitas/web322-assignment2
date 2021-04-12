6b7ac2194887afadb7a28e03633365551f847c32
Filipe Freitas <filipe.cunha.freitas@gmail.com>
2021-04-12 00:42:47 -0300

    authorization.js file update was not saved

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -10,17 +10,4 @@ const dashboardLoader = (req, res)=>
     }
 }
 
-const checkRoleAddProduct = (req, res)=>
-{
-    if (req.session.userInfo.admin == true)
-    {
-        res.render("Catalogue/addProducts");
-    }
-    else
-    {
-        res.render("User/userDashboard");
-    }
-}
-
 module.exports = dashboardLoader;
-//module.exports = checkRoleAddProduct;
